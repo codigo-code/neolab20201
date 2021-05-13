5894ee0187e00fc226a3a9591581e65966929146
dante.panella.contractor.gdevtools.com <dante.panella.contractor@gdevtools.com>
2021-05-13 18:45:19 -0300

    clase e

---
+++ frontend/clase3/js/CustomElementRegister.js
@@ -0,0 +1,20 @@
+
+
+
+class CustomElementRegister extends HTMLElement{
+    constructor(){
+        super();
+
+        this.render();
+    }
+
+    render(){
+        this.addEventListener('click',e=>{
+            e.target.style.color='red';
+        })
+    }
+
+}
+
+
+window.customElements.define('neo-custom',CustomElementRegister);
